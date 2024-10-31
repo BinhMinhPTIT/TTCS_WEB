40600959f61ecc138222b153e21776aa01d27be0
BinhMinhPTIT <binhminhtt03@gmail.com>
2024-10-31 08:59:35 +0700

    add database, Order model, cart model

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "./config/mangodb.js";
+import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoute.js";
